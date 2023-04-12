757341fb533fd28f45f57b6ee0ba864f3018a1c2
AlenKrga <47427376+AlenKrga@users.noreply.github.com>
2023-04-12 21:25:36 +0100

    added calculator and replace word python app

---
+++ replace_word/replace_word.py
@@ -0,0 +1,6 @@
+def replace_word():
+    str = "Hi everyone, nice to meet you"
+    word_to_replace = input("Enter the word to replace: ")
+    word_replacment = input("Enter the word replacment: ")
+    print(str.replace(word_to_replace, word_replacment))
+replace_word()
