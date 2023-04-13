47e4d394d21dd85ecb89959061ec0726c2353089
AlenKrga <47427376+AlenKrga@users.noreply.github.com>
2023-04-13 12:33:28 +0100

    ascii art with ANSI escape code for color change

---
+++ ASCII_art/animal.py
@@ -0,0 +1,8 @@
+print("\033[31m" + " " * 11 + "_" * 20)  # Set the color to red and print the top border of the ASCII art
+print(" " * 8 + "< Pet me and I will purr. >")  # Print the text in the middle of the ASCII art
+print("\033[31m" + " " * 11 + "-" * 20)  # Set the color to red and print the bottom border of the ASCII art
+print("\033[31m" + " " * 14 + "­­­­­­­­­­­­­­­­­­­­­­­ ")  # Set the color to red and print the cat's body
+print("\033[31m" + " " * 15 + "/")  # Set the color to red and print the cat's left paw
+print(" " * 7 + "/\_/\\ " + " /")  # Print the cat's legs and body
+print(" " * 5 + " ( o.o ) ")  # Print the cat's face
+print(" " * 6 + " > ^ < " + "\033[0m")  # Print the cat's right paw and reset the color to default
