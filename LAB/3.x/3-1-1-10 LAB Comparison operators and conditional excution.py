afd973cbb056f661d37f60607ca569d86ba641bb
AlenKrga <47427376+AlenKrga@users.noreply.github.com>
2023-04-21 01:50:44 +0100

    added lab 3xx10 operators and conditional execution

---
+++ LAB/3.x/3-1-1-10 LAB Comparison operators and conditional excution.py
@@ -0,0 +1,8 @@
+plant = input("Enter plant name: ")
+
+if plant ==  "spathiphyllum":
+    print("No, I want a big Spathiphyllum")
+elif plant == "Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+else:
+    print(f"Spathiphyllum! Not {plant}")
