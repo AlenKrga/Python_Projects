1b19aa6047a05966fd64cf756d19c6fca59edc41
AlenKrga <47427376+AlenKrga@users.noreply.github.com>
2023-04-24 22:36:39 +0100

    added comments in code

--- LAB/3.x/3-2-1-3 LAB Essentials of the while loop - Guess the secret number.py
+++ LAB/3.x/3-2-1-3 LAB Essentials of the while loop - Guess the secret number.py
@@ -1,5 +1,7 @@
+# Set the secret number
 secret_number = 777
 
+# Print the welcome message
 print(
 """
 +================================+
@@ -11,10 +13,15 @@ print(
 +================================+
 """)
 
+# Ask the user to enter their guess
 number = int(input(":"))
 
+# Keep asking the user to enter their guess until they guess the correct number
 while number != secret_number:
+    # If the user hasn't guessed the correct number yet, print a message telling them they're stuck in the loop
     print("Ha ha! You're stuck in my loop!")
+    # Ask the user to enter their guess again
     number = int(input(":"))
+# When the user finally guesses the correct number, exit the loop and print a congratulatory message
 else:
-    print("Well done, muggle! You are free now.")
+    print("Congratulations! You guessed the secret number!")
